7bae20b3cfa85897fe596dd5088d21663685128e
ComputerSchrek <larg-graffiti@outlook.de>
2025-05-31 20:38:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,27 +7,39 @@
 # This is a simple script to generate Gift Card codes
 # These are the Imports:
 import random
-# These are the Variables:
-times = 99999
+import string
+# The variables:
 codes = []
-# Now we will get to the main part of the code:
-
-
-# !DISCLAIMER!
-# This is not finished by far and Changes will be made in the future
-# The script doesn't fulfill its purpose and I will have to change it later on.
-# !DISCLAIMER!
-zwichen = input("How many codes do you want to generate?")
-if zwichen.isdigit():
+zwichen = 0
+# This script generates random Gift Card codes in the format XXXX-XXXX-XXXX
+times = 0
+# Number of codes to generate
+zwichen = input("How many codes do you want to generate? ")
+# Check if the input is a digit:
+if zwichen.isdigit(): 
     times = int(zwichen)
 else:
     print("Please enter a valid number.")
     exit()
-for i in range(times):
+# Main part of the code:
+codes = []
+for _ in range(times):
     code = ""
-    for j in range(4):
-        code += str(random.randint(0, 9))
-        if j < 3:
+    for group in range(3):  # 3 groups
+        part = ''.join(random.choices(string.ascii_uppercase + string.digits, k=4))
+        code += part
+        if group < 2:
             code += "-"
     codes.append(code)
-print(f"Generated codes:\n {len(codes)}")
+
+print(f"Generated codes ({len(codes)}):")
+for c in codes:
+    print(c)
+
+# !DISCLAIMER!
+# This Script is for educational purposes only.
+# Using this script to brute force Gift Card codes is illegal and unethical.
+# I am not responsible for any misuse of this script, as i do not condone or support any illegal activities.
+# The inputting of the codes is not even included in this script.
+# Even if you ust this to generate codes, you most probably wont be able to brute force one, as the codes are too long and complex.
+# !DISCLAIMER!
